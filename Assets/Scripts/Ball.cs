51c1983f1eb03fae2fa1e6b2d8c023be9ceff5db
Marcus <marc303i@stud.kea.dk>
2023-11-19 01:49:52 +0100

    Bolden vil nu altid blive sat tilbage i midten af banen, hvis den skulle falde under eller over et specifikt y position

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,25 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Ball : MonoBehaviour
 {
     
     public Rigidbody body;
-    
+    [FormerlySerializedAs("Football")] [SerializeField] GameObject football;
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    private Vector3 GetFootballLocalPosition() => football.transform.localPosition;
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    private void SetFootballLocalPosition(Vector3 value)
+    {
+        football.transform.localPosition = value;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var localPosition = GetFootballLocalPosition();
+        if (localPosition.y <= 0.3f || localPosition.y >= 1f)
+        {
+            SetFootballLocalPosition(Vector3.zero);
+        }
     }
-    
-    private void OnTriggerEnter(Collider other)
+
+    private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.layer == 10)
         {
@@ -40,26 +56,7 @@ public class Ball : MonoBehaviour
                 // Add an impulse forward, using the speed of the player
                 body.AddForce(forward * wall.Speed, ForceMode.Impulse);
             }
-        } else if (other.gameObject.layer == 7)
-        {
-            Debug.Log("Triggered layer 7");
-            PlayerMovement player = other.gameObject.GetComponent<PlayerMovement>();
-            Debug.Log(player);
-            if (player is not null)
-            {
-                // By subtracting the two vectors, we get a delta that
-                // 'points' from player towards this object, basically
-                // The direction we need to move forwards
-                var deltaPosition = body.transform.position - player.Position;
-                // Make sure y is flattened since we do not
-                // move up or down
-                deltaPosition.y = 0;
-                // Make it a unit direction (magnitude of 1)
-                var forward = deltaPosition.normalized;
-                // Add an impulse forward, using the speed of the player
-                body.AddForce(forward * player.Speed, ForceMode.Impulse);
-            }
-        } 
+        }
         else if (other.gameObject.layer == 8)
         {
             Debug.Log("Triggered layer 8"); 
@@ -78,8 +75,33 @@ public class Ball : MonoBehaviour
                 // Make it a unit direction (magnitude of 1)
                 var forward = deltaPosition.normalized;
                 // Add an impulse forward, using the speed of the player
-                body.AddForce(forward * enemyPlayer.Speed, ForceMode.Impulse);
+                body.AddForce(forward * (enemyPlayer.Speed * 0.5f), ForceMode.Impulse);
             }
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+         if (other.gameObject.layer == 7) 
+         {
+            Debug.Log("Triggered layer 7");
+            PlayerMovement player = other.gameObject.GetComponent<PlayerMovement>();
+            Debug.Log(player);
+            if (player is not null)
+            {
+                // By subtracting the two vectors, we get a delta that
+                // 'points' from player towards this object, basically
+                // The direction we need to move forwards
+                var deltaPosition = body.transform.position - player.Position;
+                // Make sure y is flattened since we do not
+                // move up or down
+                deltaPosition.y = 0;
+                // Make it a unit direction (magnitude of 1)
+                var forward = deltaPosition.normalized;
+                // Add an impulse forward, using the speed of the player
+                body.AddForce(forward * player.Speed, ForceMode.Impulse);
+            }
+         } 
+         
     }
 }
